4a723435c350a7de247a0b7e25fa57b3cdec6899
Naif Razzouk <b.graves_074@live.com>
2022-07-25 11:38:35 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -85,6 +85,29 @@ class Dev(Configuration):
   WSGI_APPLICATION = 'blango.wsgi.application'
 
 
+  # Logging
+  LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
+
   # CrispyForms
   CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
   CRISPY_TEMPLATE_PACK = "bootstrap5"
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, get_object_or_404, redirect
+import logging
 from django.utils import timezone
 from blog.models import Post
 from .forms import CommentForm
 
+
+logger = logging.getLogger(__name__)
+
+
 # Create your views here.
 def index(request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
+  logger.debug("this is index views.py")
+  logger.debug("Got %d posts", len(posts))
   return render(request, 'blog/index.html', {"posts": posts})
 
 def post_detail(request, slug):
+  logger.critical("this is post_detail views.py")
   post = get_object_or_404(Post, slug = slug)
 
 
